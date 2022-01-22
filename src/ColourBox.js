e848b76215243b42b98a7ac05c765e6dcbba8232
Christopher Ryan Limoa <clim0061@student.monash.edu>
2022-01-22 18:22:42 +1100

    Implement box colour change based on text

--- src/ColourBox.js
+++ src/ColourBox.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ColourBox = ({ colourName }) => {
-	return <div className="box">{colourName}</div>;
+	return (
+		<div className="box" style={{ backgroundColor: colourName }}>
+			{colourName}
+		</div>
+	);
 };
 
 export default ColourBox;
